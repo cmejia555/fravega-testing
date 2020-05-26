cd7bc8c96b8a9b45af1f2822edfb011fbc193d7e
cmejia <cesarmejia555@yahoo.com.ar>
2020-05-26 03:33:53 -0300

    Eliminacion de variables innecesarias

--- src/test/java/backend/ApiTest.java
+++ src/test/java/backend/ApiTest.java
@@ -3,15 +3,15 @@ package backend;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import org.junit.Test;
-
 import java.util.HashMap;
 import java.util.List;
+
 import static org.hamcrest.Matchers.*;
 
 
 public class ApiTest extends BaseApiTest {
 
-    //@Test
+    @Test
     public void test_01_basicApiTest() {
         String endpoint = "/breweries/autocomplete";
         HashMap<String, String> queries = new HashMap<>();
@@ -21,7 +21,7 @@ public class ApiTest extends BaseApiTest {
         response.prettyPrint();
     }
 
-    //@Test
+    @Test
     public void test_02_filterResults() {
         String filter = "Lagunitas Brewing Co";
         String endpoint = "/breweries/autocomplete";
@@ -34,20 +34,18 @@ public class ApiTest extends BaseApiTest {
         System.out.println(list);
     }
 
-    //@Test
+    @Test
     public void test_03_getDetails() {
         String filter = "Lagunitas Brewing Co";
-        String endpoint = "/breweries/autocomplete";
-        String detailsEndPoint = "/breweries/%s";
         HashMap<String, String> queries = new HashMap<>();
         queries.put("query", "lagunitas");
 
-        Response response = executeRequest(Method.GET, endpoint, queries);
+        Response response = executeRequest(Method.GET, "/breweries/autocomplete", queries);
 
         List<HashMap<String, String>> list = response.body().jsonPath().param("barName", filter).getList("findAll{it -> it.name == barName}");
 
         for (HashMap<String, String> map : list) {
-            Response detailsResponse = executeRequest(Method.GET, String.format(detailsEndPoint, map.get("id")));
+            Response detailsResponse = executeRequest(Method.GET, "/breweries/" + map.get("id"));
             detailsResponse.prettyPrint();
         }
     }
@@ -55,18 +53,15 @@ public class ApiTest extends BaseApiTest {
     @Test
     public void test_04_validateBrewery() {
         String filter = "Lagunitas Brewing Co";
-        String endpoint = "/breweries/autocomplete";
-        String detailsEndPoint = "/breweries/%s";
         HashMap<String, String> queries = new HashMap<>();
         queries.put("query", "lagunitas");
 
-        Response response = executeRequest(Method.GET, endpoint, queries);
+        Response response = executeRequest(Method.GET, "/breweries/autocomplete", queries);
 
         List<HashMap<String, String>> list = response.body().jsonPath().param("breweryName", filter).getList("findAll{it -> it.name == breweryName}");
 
         for (HashMap<String, String> map : list) {
-            Response detailsResponse = executeRequest(Method.GET, String.format(detailsEndPoint, map.get("id")));
-
+            Response detailsResponse = executeRequest(Method.GET, "/breweries/" + map.get("id"));
             if (detailsResponse.path("state").equals("California")) {
                 detailsResponse.then()
                         .body("id", equalTo(761))
