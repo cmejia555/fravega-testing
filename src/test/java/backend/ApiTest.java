ad08966eb558f79c2660fc77c90694206d95a980
cmejia <cesarmejia555@yahoo.com.ar>
2020-05-26 01:58:40 -0300

    Test filtro items

--- src/test/java/backend/ApiTest.java
+++ src/test/java/backend/ApiTest.java
@@ -5,6 +5,7 @@ import io.restassured.response.Response;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.List;
 
 
 public class ApiTest extends BaseApiTest {
@@ -17,5 +18,19 @@ public class ApiTest extends BaseApiTest {
 
         Response response = executeRequest(Method.GET, endpoint, queries);
         response.prettyPrint();
+    }
+
+    @Test
+    public void test_02_filterResults() {
+        String filter = "Lagunitas Brewing Co";
+        String endpoint = "/breweries/autocomplete";
+        HashMap<String, String> queries = new HashMap<>();
+        queries.put("query", "lagunitas");
+
+
+        Response response = executeRequest(Method.GET, endpoint, queries);
+
+        List<String> list = response.body().jsonPath().param("name", filter).getList("findAll{it -> it.name == name}");
+        System.out.println(list);
     }
 }
