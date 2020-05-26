401eeaea9b8e05dd118bea58ab507394e7becc2e
cmejia <cesarmejia555@yahoo.com.ar>
2020-05-25 23:29:30 -0300

    Test terminados

--- src/test/java/FravegaTest.java
+++ src/test/java/FravegaTest.java
@@ -9,8 +9,10 @@ public class FravegaTest extends TestBase {
     @Test
     public void test_01_searchSomething() {
         String product = "Heladera";
+
         HomePage homePage = new HomePage();
         homePage.searchProduct(product);
+
         ProductsPage productsPage = new ProductsPage();
         productsPage.validateSearchedProduct(product);
     }
@@ -19,11 +21,18 @@ public class FravegaTest extends TestBase {
     public void test_02_filterProduct() {
         String product = "Heladera";
         String categoryFilter = "Heladeras, Freezers y Cavas";
+        String brandFilter = "Whirlpool";
+        int itemsByPage = 15;
+
         HomePage homePage = new HomePage();
         homePage.searchProduct(product);
+
         ProductsPage productsPage = new ProductsPage();
         productsPage.validateSearchedProduct(product);
         productsPage.filterByCategory(categoryFilter);
+        productsPage.filterByBrand(brandFilter);
+        productsPage.validateItemsGrid(itemsByPage, brandFilter);
+        productsPage.validateAppliedCategoryFilter(categoryFilter);
     }
 
 }
--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -1,6 +1,5 @@
 package pages;
 
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.openqa.selenium.By;
@@ -8,26 +7,27 @@ import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
-import static org.hamcrest.Matchers.contains;
 
 public class ProductsPage extends BasePage {
-    private final By sideBar = By.name("aggregations");
+    private final By sideBarSection = By.name("aggregations");
     private final By itemsGridSection = By.name("itemsGrid");
     private final By sideBarTitle = By.name("categoryTitle");
     private String categoryTitleTemplate = "//li/a/h3[text()='%s']";
     private String brandCheckBoxTemplate = "//li[@name='brandAggregation']/a/label[text()='%s']";
     private By items = By.xpath("//ul[@name='itemsGrid']/li");
+    private By breadcrumb = By.xpath("//div[@name='breadcrumb']/ul/li/a[@cursor='default']");
 
     public ProductsPage() {
         validatePage();
     }
 
     private void validatePage() {
-        Assert.assertTrue("No se encontro el menu lateral", exists(sideBar));
+        Assert.assertTrue("No se encontro el menu lateral", exists(sideBarSection));
         Assert.assertTrue("No se encontro la grilla de items con los productos", exists(itemsGridSection));
     }
 
     public void validateSearchedProduct(String product) {
+        Assert.assertTrue("No se encontro el titulo del menu lateral", exists(sideBarTitle));
         Assert.assertEquals("El menu lateral deberia tener como titulo: " + product, product, getText(sideBarTitle));
     }
 
@@ -46,11 +46,19 @@ public class ProductsPage extends BasePage {
     public void validateItemsGrid(int gridSize, String itemTitle) {
         Assert.assertTrue("No se encontraron items en la grilla", exists(items));
         List<WebElement> itemList = findElements(items);
+
+        Assert.assertEquals("El tamaño de la grilla no coincide con lo que se muestra en pantalla",
+                gridSize, itemList.size());
+
         for (WebElement item: itemList) {
             String text = item.findElement(By.xpath(".//article/div/h4")).getText();
             Assert.assertThat("El item no contiene el titulo: " + itemTitle, text, Matchers.containsString(itemTitle));
         }
-        Assert.assertEquals("El tamaño de la grilla no coincide con lo que se muestra en pantalla",
-                gridSize, itemList.size());
+    }
+
+    public void validateAppliedCategoryFilter(String filter) {
+        Assert.assertTrue("No se encontro el breadcrumb", exists(breadcrumb));
+        Assert.assertEquals("En el breadcrumb de la pagina deberia visualizarse: " + filter,
+                filter, getText(breadcrumb));
     }
 }
